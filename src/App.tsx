ca0039a76ee60472ab6b3f6b9a5bf9c524136e11
aravind <aravindmohanan321@gmail.com>
2023-10-15 18:29:24 +0530

    reverted star canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Hero, Navbar, Tech, Works } from './components';
+import { About, Contact, Experience, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
 
 const App = () => (
   <BrowserRouter>
@@ -15,7 +15,7 @@ const App = () => (
       {/* <Feedbacks /> */}
       <div className="relative z-0">
         <Contact />
-        {/* <StarsCanvas /> */}
+        <StarsCanvas />
       </div>
     </div>
   </BrowserRouter>

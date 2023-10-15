8b4d5de3e76e1d249d4b102f6063b511a925db7c
aravind <aravindmohanan321@gmail.com>
2023-10-15 19:35:28 +0530

    computer canvas fix

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -1,36 +1,45 @@
 import React, { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload } from '@react-three/drei';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-// interface ComputersProps {
-//   isMobile: boolean;
-// }
+interface ComputersProps {
+  isMobile: boolean;
+}
 
-// const Computers = ({ isMobile }: ComputersProps): React.JSX.Element => {
-//   const computer = useGLTF('./desktop_pc/scene.gltf');
-//   return (
-//     <mesh>
-//       <hemisphereLight intensity={0.15} groundColor="black" />
-//       <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
-//       <pointLight intensity={1} />
-//       <primitive
-//         object={computer.scene}
-//         scale={isMobile ? 0.7 : 0.75}
-//         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-//         rotation={[-0.01, -0.2, -0.1]}
-//       />
-//     </mesh>
-//   );
-// };
+const Computers = ({ isMobile }: ComputersProps): React.JSX.Element => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
 
 const ComputersCanvas = (): React.JSX.Element => {
   const [isMobile, setIsMobile] = useState(false);
-  isMobile
+
+  const isLoggedFromMobile = (): boolean=>{
+    const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    return regex.test(navigator.userAgent);
+  }
+
   useEffect(() => {
     const mediaQuery = window.matchMedia('(max-width: 500px)');
     setIsMobile(mediaQuery.matches);
 
+    if (isLoggedFromMobile()) {
+      setIsMobile(true);
+    }
+    
     const handleMediaQueryChanges = (event: MediaQueryListEvent) => {
       setIsMobile(event.matches);
     };
@@ -51,7 +60,7 @@ const ComputersCanvas = (): React.JSX.Element => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-        {/* <Computers isMobile={isMobile} /> */}
+        <Computers isMobile={isMobile} />
         <Preload all />
       </Suspense>
     </Canvas>

260f46a56b731f4712dad8be53af075ad4965cdf
aravind <aravindmohanan321@gmail.com>
2023-10-15 20:25:23 +0530

    changes

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -4,7 +4,7 @@ import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.cjs';
 
 const Stars = (props: any): React.JSX.Element => {
-  const ref = useRef<THREE.Points>(null);
+  const ref = useRef<any>(null);
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
   useFrame((_, delta) => {

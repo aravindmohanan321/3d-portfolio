a69c38a7f6e3cc514eddb409949c5f379f780d28
aravind <aravindmohanan321@gmail.com>
2023-10-24 22:41:51 +0530

    computer model

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -8,7 +8,7 @@ interface ComputersProps {
 }
 
 const Computers = ({ isMobile }: ComputersProps): React.JSX.Element => {
-  const computer = useGLTF('./gaming_desktop_pc_blend_file.glb');
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
       <hemisphereLight intensity={4} groundColor="black" />
